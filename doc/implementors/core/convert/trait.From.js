a9be62f909b524f24a211863f3c1f0ff47837907
0xSilene <0xsilene@MacBook.local>
2019-10-17 21:21:03 -0400

    Added docs

---
+++ doc/implementors/core/convert/trait.From.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["lib"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;[u8; 32]&gt; for <a class=\"struct\" href=\"lib/struct.U256.html\" title=\"struct lib::U256\">U256</a>",synthetic:false,types:["lib::U256"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;[u8; 48]&gt; for <a class=\"struct\" href=\"lib/struct.U384.html\" title=\"struct lib::U384\">U384</a>",synthetic:false,types:["lib::U384"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;[u8; 64]&gt; for <a class=\"struct\" href=\"lib/struct.U512.html\" title=\"struct lib::U512\">U512</a>",synthetic:false,types:["lib::U512"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;[u8; 128]&gt; for <a class=\"struct\" href=\"lib/struct.U1024.html\" title=\"struct lib::U1024\">U1024</a>",synthetic:false,types:["lib::U1024"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;[u8; 256]&gt; for <a class=\"struct\" href=\"lib/struct.U2048.html\" title=\"struct lib::U2048\">U2048</a>",synthetic:false,types:["lib::U2048"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
